67219a508d0a0139bb8323c0f54e45e6643c03aa
shovelheadfxe <scottcarnley@gmail.com>
2015-03-06 02:28:34 -0500

    Answers

--- public/modules/answers/controllers/answers.client.controller.js
+++ public/modules/answers/controllers/answers.client.controller.js
@@ -1,8 +1,13 @@
 'use strict';
 
-angular.module('answers').controller('AnswersController', ['$scope','$stateParams','Questions',
-	function($scope,$stateParams,Questions) {
+angular.module('answers').controller('AnswersController', ['$scope','$stateParams','$location','Questions',
+	function($scope,$stateParams,$location,Questions) {
 		var ques = Questions.get({questionId: $stateParams.questionId});
 		ques.$promise.then(function(q){$scope.question = q;}, function(e){console.log(JSON.stringify(e));});
+
+		$scope.continue = function(){
+			$location.path('/quiz');
+			$scope.$apply();
+		};
 	}
 ]);
